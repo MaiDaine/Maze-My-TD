2d250096a94c0eec0959cf1c771e5ec249e0e152
David Maidenberg <david.maidenberg@epitech.eu>
2019-09-04 21:45:34 +0200

    Add GridManager class and its custom editor to create and destroy grid

---
+++ Assets/Scripts/Grid/Editor/GridEditor.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace MazeMyTD
+{
+    public class GridEditor : EditorWindow
+    {
+        public Tile.TileState tileOptions;
+
+        [MenuItem("Window/GridEditor")]
+        public static void ShowWindow()
+        {
+            GetWindow<GridEditor>("GridEditor");
+        }
+
+        private void OnGUI()
+        {
+
+#if UNITY_EDITOR
+            //Grid Construction
+            if (GUILayout.Button("Spawn Grid"))
+                FindObjectOfType<GridManager>().SpawnGrid();
+            if (GUILayout.Button("Refresh Grid"))
+                FindObjectOfType<GridManager>().UpdateTilePosition();
+            if (GUILayout.Button("Destroy Grid"))
+                FindObjectOfType<GridManager>().DestroyGrid();
+
+            EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+
+            //Tile Selection
+            tileOptions = (Tile.TileState)EditorGUILayout.EnumPopup("Type to select:", tileOptions);
+            if (GUILayout.Button("Select"))
+            {
+                List<Tile> result = ArrayUtility.FindAll(FindObjectsOfType<Tile>(), (t) => t.tileState == tileOptions);
+                GameObject[] results = new GameObject[result.Count];
+
+                int i = 0;
+                foreach (Tile tile in result)
+                {
+                    results[i] = tile.gameObject;
+                    i++;
+                }
+
+                Selection.objects = results;
+            }
+
+            EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+        }
+#endif
+    }
+}
---
+++ Assets/Scripts/Grid/GridManager.cs
@@ -0,0 +1,59 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace MazeMyTD
+{
+    public class GridManager : MonoBehaviour
+    {
+        public GridData gd;
+
+        public void SpawnGrid()
+        {
+#if UNITY_EDITOR
+            DestroyGrid();
+            gd.tiles = new GameObject[gd.height * gd.width];
+            for (int z = 0; z < gd.height; z++)
+                for (int x = 0; x < gd.width; x++)
+                {
+                    GameObject tile = (GameObject)PrefabUtility.InstantiatePrefab(gd.tileRef as GameObject, this.transform);
+                    gd.tiles[z * gd.width + x] = tile;
+                    tile.transform.position = new Vector3(transform.position.x + gd.tileOffset * x, transform.position.y, transform.position.z + gd.tileOffset * z);
+                    tile.GetComponent<Tile>().gridPos = new Vector2Int(x, z);
+                }
+#endif
+        }
+
+        public void UpdateTilePosition()
+        {
+            if (gd.tiles != null && gd.tiles.Length != 0)
+                foreach (GameObject tileObj in gd.tiles)
+                {
+                    Tile tile = tileObj.GetComponent<Tile>();
+                    tileObj.transform.position = new Vector3(transform.position.x + gd.tileOffset * tile.gridPos.x, transform.position.y, transform.position.z + gd.tileOffset * tile.gridPos.y);
+                }
+            else
+            {
+                Tile[] tiles = FindObjectsOfType<Tile>();
+                if (tiles.Length == 0)
+                {
+                    Debug.Log("No tiles found, please re spawn the grid");
+                    return;
+                }
+
+                gd.tiles = null;
+                gd.tiles = new GameObject[tiles.Length];
+                foreach (Tile tile in tiles)
+                    gd.tiles[tile.gridPos.y * gd.width + tile.gridPos.x] = tile.gameObject;
+            }
+        }
+
+        public void DestroyGrid()
+        {
+            if (gd.tiles != null && gd.tiles.Length != 0)
+                for (int z = 0; z < gd.height; z++)
+                    for (int x = 0; x < gd.width; x++)
+                        DestroyImmediate(gd.tiles[z * gd.width + x]);
+            gd.tiles = null;
+        }
+    }
+}
