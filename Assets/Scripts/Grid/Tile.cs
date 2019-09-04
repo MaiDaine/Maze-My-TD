74341923758c97d2a03244fa040b561f4aadb306
David Maidenberg <david.maidenberg@epitech.eu>
2019-09-04 21:26:00 +0200

    Add Tile class to store individual tile state and a Tile prefab

---
+++ Assets/Scripts/Grid/Tile.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace MazeMyTD
+{
+    public class Tile : MonoBehaviour
+    {
+        public enum TileState { Invalid, Empty, Occupied};
+
+        public TileState tileState = TileState.Empty;
+        public Vector2Int gridPos;
+        public Transform tileCenter;
+    }
+}
