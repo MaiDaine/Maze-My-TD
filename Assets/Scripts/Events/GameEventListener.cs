9579098305d347b47cf96b9f473cb296277004e9
David Maidenberg <david.maidenberg@epitech.eu>
2019-09-06 01:07:20 +0200

    Add custom GameEvent, GameEventListener, and events for playerData change

---
+++ Assets/Scripts/Events/GameEventListener.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace MazeMyTD
+{
+    public class GameEventListener : MonoBehaviour
+    {
+        public GameEvent gameEvent;
+        public UnityEvent response;
+
+        private void OnEnable()
+        {
+            gameEvent.RegisterListener(this);
+        }
+
+        private void OnDisable()
+        {
+            gameEvent.UnregisterListener(this);
+        }
+
+        public void OnEventRaised()
+        {
+            response.Invoke();
+        }
+    }
+}
