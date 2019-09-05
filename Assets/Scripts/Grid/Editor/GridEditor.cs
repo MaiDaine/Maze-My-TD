99773b11945d65f1965ec85c688a1d8fe7b1d636
David Maidenberg <david.maidenberg@epitech.eu>
2019-09-05 16:24:06 +0200

    Add InnateElement class for level design and its first prefab: Obstacle

--- Assets/Scripts/Grid/Editor/GridEditor.cs
+++ Assets/Scripts/Grid/Editor/GridEditor.cs
@@ -22,7 +22,10 @@ namespace MazeMyTD
             if (GUILayout.Button("Spawn Grid"))
                 FindObjectOfType<GridManager>().SpawnGrid();
             if (GUILayout.Button("Refresh Grid"))
+            {
                 FindObjectOfType<GridManager>().UpdateTilePosition();
+                SetTileStatusOnInnateBuildings();
+            }
             if (GUILayout.Button("Destroy Grid"))
                 FindObjectOfType<GridManager>().DestroyGrid();
 
@@ -31,21 +34,31 @@ namespace MazeMyTD
             //Tile Selection
             tileOptions = (Tile.TileState)EditorGUILayout.EnumPopup("Type to select:", tileOptions);
             if (GUILayout.Button("Select"))
-            {
-                List<Tile> result = ArrayUtility.FindAll(FindObjectsOfType<Tile>(), (t) => t.tileState == tileOptions);
-                GameObject[] results = new GameObject[result.Count];
-
-                int i = 0;
-                foreach (Tile tile in result)
-                {
-                    results[i] = tile.gameObject;
-                    i++;
-                }
-
-                Selection.objects = results;
-            }
+                TileSelection(tileOptions);
 
             EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+        }
+
+        private void SetTileStatusOnInnateBuildings()
+        {
+           InnateElement[] innateElements = FindObjectsOfType<InnateElement>();
+            for (int i = 0; i < innateElements.Length; i++)
+                innateElements[i].SetInvalidStatusOnTile();
+        }
+
+        private void TileSelection(Tile.TileState tileState)
+        {
+            List<Tile> result = ArrayUtility.FindAll(FindObjectsOfType<Tile>(), (t) => t.tileState == tileState);
+            GameObject[] results = new GameObject[result.Count];
+
+            int i = 0;
+            foreach (Tile tile in result)
+            {
+                results[i] = tile.gameObject;
+                i++;
+            }
+
+            Selection.objects = results;
         }
 #endif
     }
---
+++ Assets/Scripts/Grid/InnateElement.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace MazeMyTD
+{
+    public class InnateElement : MonoBehaviour
+    {
+        public Tile occupiedTile;
+
+        public void SetInvalidStatusOnTile()
+        {
+            occupiedTile.tileState = Tile.TileState.Invalid;
+        }
+    }
+}
